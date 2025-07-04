aa5ad1a340c1313278e82d72b9b53956bb4ae946
cvlnpsg987 <cvlnpsg987@gmail.com>
2025-07-04 12:15:06 +0000

    startServer func added

--- server/src/server.js
+++ server/src/server.js
@@ -4,10 +4,15 @@ import { connectDB } from './config/db.js';
 
 const app = express();
 
-connectDB();
-
-const port = 8080
-
-app.listen(ENV.PORT, () => console.log(`Listening on http://localhost:${ENV.PORT}`));
-
 app.get("/", (req, res) => res.send("Working"));
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => console.log(`Listening on http://localhost:${ENV.PORT}`));
+    } catch (error) {
+        console.log("Failed to start the server:", error.message);
+    }
+};
+
+startServer();
