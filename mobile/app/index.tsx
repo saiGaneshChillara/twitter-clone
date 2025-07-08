491c87732dea08e99d3a1567583c96de40330e82
cvlnpsg987 <cvlnpsg987@gmail.com>
2025-07-08 10:46:26 +0000

    unwanted changes

---
+++ mobile/app/index.tsx
@@ -0,0 +1,32 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+/*
+expo@53.0.16 - expected version: 53.0.17
+  expo-constants@17.1.6 - expected version: ~17.1.7
+  expo-linking@7.1.6 - expected version: ~7.1.7
+  expo-router@5.1.2 - expected version: ~5.1.3
+  expo-splash-screen@0.30.9 - expected version: ~0.30.10
+*/
+
+/* 
+The following packages should be updated for best compatibility with the installed expo version:
+  expo@53.0.16 - expected version: 53.0.17
+  expo-constants@17.1.6 - expected version: ~17.1.7
+  expo-linking@7.1.6 - expected version: ~7.1.7
+  expo-router@5.1.2 - expected version: ~5.1.3
+  expo-splash-screen@0.30.9 - expected version: ~0.30.10
+Your project may not work correctly until you install the expected versions of the packages.
+*/
